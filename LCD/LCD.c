9254e38c7484bd37c10d9e221db919bec00a2be1
Tyler Ryan <tjrg88@mst.edu>
2014-10-05 22:45:29 -0500

    Added files for LCD functions

---
+++ LCD/LCD.c
@@ -0,0 +1,53 @@
+#include <LCD.h>
+#include <delay.h>
+
+void LCD_Init(void)
+{
+	Delay_ms(30);
+	LCD_Write(COMMAND, FUNCTION_SET);
+	Delay_ms(5);
+	LCD_Write(COMMAND, FUNCTION_SET);
+	Delay_ms(1);
+	LCD_Write(COMMAND, FUNCTION_SET);
+	Delay_ms(1);
+	LCD_Write(COMMAND, DISPLAY_CONTROL);
+	Delay_ms(1);
+	LCD_Write(COMMAND, DISPLAY_CLEAR);
+	Delay_ms(2);
+	LCD_Write(COMMAND, ENTRY_MODE_SET);
+	Delay_ms(1);
+}
+
+void LCD_Write(bit REG_SELECT, uint8 LCD_Data)
+{
+	RS = REG_SELECT;
+	E = 1;
+	P0 = LCD_Data;
+	E = 0;
+	P0 = 0xFF;
+}
+
+void LCD_Print(uint8 num_bytes, uint8 * array_name)
+{
+	uint8 value, index;
+	if(num_bytes == 0)
+	{
+		value = *array_name;
+		index = 0;
+		while(value != NUL)
+		{
+			LCD_Write(DDRAM, value);
+			index++;
+			value = *(array_name + index);
+		}
+	}
+	else
+	{
+		value = *array_name;
+		for(index = 0; index < num_bytes && value != NUL; index++)
+		{
+			LCD_Write(DDRAM, value);
+			value = *(array_name + index + 1);
+		}
+	}
+}
---
+++ LCD/LCD.h
@@ -0,0 +1,17 @@
+#ifndef _LCD_H
+#define _LCD_H
+#include <Main.h>
+#include <PORT.h>
+
+#define NUL (0x00)
+#define COMMAND (0)
+#define DDRAM (1)
+#define FUNCTION_SET (0x38) // set 8-bit interface, 2 line mode, 5x8 font
+#define DISPLAY_CONTROL (0x0F) // set display on, cursor on, cursor blink on
+#define DISPLAY_CLEAR (0x01) // replace all characters in display with spaces
+#define ENTRY_MODE_SET (0x04) // set auto increment DDRAM address, shift cursor
+
+void LCD_Init(void);
+void LCD_Write(bit REG_SELECT, uint8 LCD_Data);
+void LCD_Print(uint8 num_bytes, uint8 * array_name);
+#endif
